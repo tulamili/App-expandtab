2276c67c0919614cfb0536dceefc3156f3da781b
Toshiyuki Shimono <bin4tsv@gmail.com>
2022-10-19 22:19:11 +0900

    0.030 を作成。初めて、テストを行う -D を実装。

--- expandtab.pm
+++ expandtab.pm
@@ -1,6 +1,6 @@
 package App::expandtab ;  
-our $VERSION = '0.023' ; 
-our $DATE = '2022-10-19T16:45+09:00' ; 
+our $VERSION = '0.030' ; 
+our $DATE = '2022-10-19T22:17+09:00' ; 
 
 =encoding utf8
 
