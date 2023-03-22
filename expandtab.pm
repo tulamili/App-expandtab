352f831d4feb09002ae090fcf4d09928ac3a496b
Toshiyuki Shimono <bin4tsv@gmail.com>
2023-03-22 19:23:53 +0900

    0.042 を作るべく Changes とexpandtab.pm に加筆。

--- expandtab.pm
+++ expandtab.pm
@@ -1,6 +1,6 @@
 package App::expandtab ;  
-our $VERSION = '0.041' ; 
-our $DATE = '2023-03-22T18:37+09:00' ; 
+our $VERSION = '0.042' ; 
+our $DATE = '2023-03-22T19:23+09:00' ; 
 
 =encoding utf8
 
